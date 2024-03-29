d4e43c349de9d4a194d6fb37a31d37c285deb360
JuanDuran85 <duran85juan@gmail.com>
2024-03-28 23:12:40 -0300

    theme app

--- yes_no_app/lib/presentation/widgets/shared/message_field_box.dart
+++ yes_no_app/lib/presentation/widgets/shared/message_field_box.dart
@@ -5,6 +5,43 @@ class MessageFieldBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text('Hola mundo');
+    final outlineInputBorder = UnderlineInputBorder(
+      borderSide: const BorderSide(
+        color: Colors.transparent,
+      ),
+      borderRadius: BorderRadius.circular(40),
+    );
+
+    final focusNode = FocusNode();
+    final textController = TextEditingController();
+
+    final inputDecoration = InputDecoration(
+      hintText: 'End your message with a "??"',
+      enabledBorder: outlineInputBorder,
+      focusedBorder: outlineInputBorder,
+      filled: true,
+      suffixIcon: IconButton(
+        icon: const Icon(
+          Icons.send_outlined,
+        ),
+        onPressed: () {
+          final textValue = textController.value.text;
+          print('---------------------------Text value $textValue');
+          textController.clear();
+        },
+      ),
+    );
+
+    return TextFormField(
+      onTapOutside: (_) => focusNode.unfocus(),
+      focusNode: focusNode,
+      controller: textController,
+      decoration: inputDecoration,
+      onFieldSubmitted: (value) {
+        print('--------------------------Submit value: $value');
+        textController.clear();
+        focusNode.requestFocus();
+      },
+    );
   }
 }
