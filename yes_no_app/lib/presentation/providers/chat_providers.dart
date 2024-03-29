107ea103839920430943711663b91d12038a483b
JuanDuran85 <duran85juan@gmail.com>
2024-03-29 16:46:31 -0300

    functionality app chat

--- yes_no_app/lib/presentation/providers/chat_providers.dart
+++ yes_no_app/lib/presentation/providers/chat_providers.dart
@@ -2,10 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
-  List<Message> message = [
+  List<Message> messageList = [
     Message(text: 'Nuevo mensaje', fromWho: FromWho.me),
     Message(text: 'Es un nuevo mensaje?', fromWho: FromWho.me),
   ];
 
-  Future<void> sendMessage(String text) async {}
+  final ScrollController chatScrollController = ScrollController();
+
+  Future<void> sendMessage(String text) async {
+    if (text.isEmpty) return;
+    final newMessage = Message(text: text, fromWho: FromWho.me);
+    messageList.add(newMessage);
+    notifyListeners();
+    moveScrollToBottom();
+  }
+
+  void moveScrollToBottom() async {
+    await Future.delayed(const Duration(milliseconds: 100));
+
+    chatScrollController.animateTo(
+      chatScrollController.position.maxScrollExtent,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeOut,
+    );
+  }
 }
--- yes_no_app/lib/presentation/widgets/shared/message_field_box.dart
+++ yes_no_app/lib/presentation/widgets/shared/message_field_box.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class MessageFieldBox extends StatelessWidget {
-  const MessageFieldBox({super.key});
+  final ValueChanged<String> onValue;
+
+  const MessageFieldBox({super.key, required this.onValue});
 
   @override
   Widget build(BuildContext context) {
@@ -26,8 +28,8 @@ class MessageFieldBox extends StatelessWidget {
         ),
         onPressed: () {
           final textValue = textController.value.text;
-          print('---------------------------Text value $textValue');
           textController.clear();
+          onValue(textValue);
         },
       ),
     );
@@ -38,9 +40,9 @@ class MessageFieldBox extends StatelessWidget {
       controller: textController,
       decoration: inputDecoration,
       onFieldSubmitted: (value) {
-        print('--------------------------Submit value: $value');
         textController.clear();
         focusNode.requestFocus();
+        onValue(value);
       },
     );
   }
