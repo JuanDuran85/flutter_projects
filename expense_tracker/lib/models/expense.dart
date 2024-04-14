51f88bd7af9abdaf5a64c5217d7d3ada84e202a2
JuanDuran85 <duran85juan@gmail.com>
2024-04-13 20:38:23 -0400

    add icons and colums

--- expense_tracker/lib/models/expense.dart
+++ expense_tracker/lib/models/expense.dart
@@ -1,9 +1,22 @@
 // ignore: depend_on_referenced_packages
+import 'package:flutter/material.dart';
+// ignore: depend_on_referenced_packages
 import 'package:uuid/uuid.dart';
+// ignore: depend_on_referenced_packages
+import 'package:intl/intl.dart';
 
+final formatter = DateFormat.yMd();
 const uuid = Uuid();
 
 enum Category { food, travel, leisure, work, others }
+
+const categoryIcons = {
+  Category.food: Icons.lunch_dining,
+  Category.travel: Icons.flight_takeoff,
+  Category.leisure: Icons.movie,
+  Category.work: Icons.work,
+  Category.others: Icons.question_mark_outlined,
+};
 
 class Expense {
   final String title;
@@ -18,4 +31,8 @@ class Expense {
     required this.date,
     required this.category,
   }) : id = uuid.v4();
+
+  String get formattedDate {
+    return formatter.format(date);
+  }
 }
