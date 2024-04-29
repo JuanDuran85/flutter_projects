c7d83b8c65c592c43822402130e2c1e9f9317f12
JuanDuran85 <duran85juan@gmail.com>
2024-04-28 20:13:10 -0400

    dialogue widget

--- widget_app/lib/presentation/screens/snackbar/snackbar_screen.dart
+++ widget_app/lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+// ignore: depend_on_referenced_packages
+import 'package:go_router/go_router.dart';
 
 class SnackbarScreen extends StatelessWidget {
   static const name = 'snackbar_screen';
@@ -15,14 +17,61 @@ class SnackbarScreen extends StatelessWidget {
     ScaffoldMessenger.of(context).showSnackBar(snackBarToShow);
   }
 
+  void openDialog(BuildContext context) {
+    showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Licencias Usadas'),
+        content: const Text(
+          'Enim proident non aliqua deserunt nostrud Lorem ex velit qui aliqua. Tempor in veniam mollit occaecat esse aliquip elit laboris eu reprehenderit culpa. Consectetur ut labore id irure mollit reprehenderit amet. Elit irure occaecat reprehenderit elit ad amet aliqua velit. Fugiat Lorem voluptate sunt exercitation.',
+        ),
+        actions: [
+          TextButton(
+            onPressed: () => context.pop(),
+            child: const Text(
+              'Cancel',
+            ),
+          ),
+          FilledButton(
+            onPressed: () => context.pop(),
+            child: const Text(
+              'Ok',
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Snackbar Screen'),
       ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FilledButton.tonal(
+              onPressed: () {
+                showAboutDialog(context: context, children: [
+                  const Text(
+                      'lorem ipsum dolor sit amet consectetur adipiscing elit in non laoreet nibh. Nostrud excepteur labore ut nostrud nostrud sint.'),
+                ]);
+              },
+              child: const Text('Licencias Usadas'),
+            ),
+            FilledButton.tonal(
+              onPressed: () => openDialog(context),
+              child: const Text('Mostrar diálogo'),
+            ),
+          ],
+        ),
+      ),
       floatingActionButton: FloatingActionButton.extended(
-        onPressed: ()  => showCustomSnackbar(context),
+        onPressed: () => showCustomSnackbar(context),
         label: const Text('Snackbar Show'),
         icon: const Icon(Icons.remove_red_eye_outlined),
       ),
