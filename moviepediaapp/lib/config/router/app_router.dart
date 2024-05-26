a956fc1b85c9255e668e1bc6c4ea47b85c81ff8e
JuanDuran85 <duran85juan@gmail.com>
2024-05-26 17:59:15 -0400

    basic config app movie

---
+++ moviepediaapp/lib/config/router/app_router.dart
@@ -0,0 +1,11 @@
+// ignore: depend_on_referenced_packages
+import 'package:go_router/go_router.dart';
+import 'package:moviepediaapp/presentation/screens/screens.dart';
+
+final appRouter = GoRouter(initialLocation: '/', routes: [
+  GoRoute(
+    path: '/',
+    name: HomeScreen.name,
+    builder: (context, state) => const HomeScreen(),
+  )
+]);
--- moviepediaapp/lib/main.dart
+++ moviepediaapp/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+
+import 'package:moviepediaapp/config/router/app_router.dart';
 import 'package:moviepediaapp/config/theme/app_theme.dart';
 
 void main() {
@@ -10,14 +12,10 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig: appRouter,
       debugShowCheckedModeBanner: false,
       theme: AppTheme().getTheme(),
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
     );
   }
 }
