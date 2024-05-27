2c5f2d3efd156c1b583941cd9a309e931373994e
JuanDuran85 <duran85juan@gmail.com>
2024-05-27 18:09:39 -0400

    working with infra data

--- moviepediaapp/lib/infrastructure/datasources/tmdb_datasource.dart
+++ moviepediaapp/lib/infrastructure/datasources/tmdb_datasource.dart
@@ -1,8 +1,12 @@
+
 // ignore: depend_on_referenced_packages
 import 'package:dio/dio.dart';
+
 import 'package:moviepediaapp/config/constants/enviroment.dart';
 import 'package:moviepediaapp/domain/datasources/movies_datasource.dart';
 import 'package:moviepediaapp/domain/entities/movie_entities.dart';
+import 'package:moviepediaapp/infrastructure/mappers/movie_mapper.dart';
+import 'package:moviepediaapp/infrastructure/models/themoviedb/tmdb_response_model.dart';
 
 class TheMovieDbDatasource extends MoviesDataSource {
   final dio = Dio(
@@ -12,11 +16,15 @@ class TheMovieDbDatasource extends MoviesDataSource {
   }));
   @override
   Future<List<Movie>> getNowPlayingMovies({int page = 1}) async {
-    final response = await dio.get('/movie/now_playing');
+    final Response<dynamic> response = await dio.get('/movie/now_playing');
 
-    
+    final TheMovieDbResponse movieDbResponse =
+        TheMovieDbResponse.fromJson(response.data);
+    final List<Movie> finalMovies = movieDbResponse.results
+        .where((moviedb) => moviedb.posterPath != 'no-poster')
+        .map((movieDb) => MovieMapper.theMovieDbToEntity(movieDb))
+        .toList();
 
-
-    return [];
+    return finalMovies;
   }
 }
--- moviepediaapp/lib/infrastructure/mappers/movie_mapper.dart
+++ moviepediaapp/lib/infrastructure/mappers/movie_mapper.dart
@@ -18,7 +18,7 @@ class MovieMapper {
         popularity: movieDb.popularity,
         posterPath: movieDb.posterPath != ''
             ? 'https://image.tmdb.org/t/p/w500${movieDb.posterPath}'
-            : noImageFound,
+            : 'no-poster',
         releaseDate: movieDb.releaseDate,
         title: movieDb.title,
         video: movieDb.video,
