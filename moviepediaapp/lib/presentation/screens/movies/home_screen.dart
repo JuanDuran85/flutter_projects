d055d334eafed798c8e886d3991dea0c2239ff77
JuanDuran85 <duran85juan@gmail.com>
2024-05-28 16:36:44 -0400

    home screen

--- moviepediaapp/lib/presentation/screens/movies/home_screen.dart
+++ moviepediaapp/lib/presentation/screens/movies/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 // ignore: depend_on_referenced_packages
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:moviepediaapp/presentation/providers/movies/movies_providers.dart';
+import 'package:moviepediaapp/presentation/widgets/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   static const name = 'home-screen';
@@ -35,14 +36,21 @@ class _HomeViewState extends ConsumerState<_HomeView> {
   @override
   Widget build(BuildContext context) {
     final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
-    return ListView.builder(
-      itemCount: nowPlayingMovies.length,
-      itemBuilder: (context, index) {
-        final movie = nowPlayingMovies[index];
-        return ListTile(
-          title: Text(movie.title),
-        );
-      },
+    return Column(
+      children: [
+        CustomAppBar(),
+        Expanded(
+          child: ListView.builder(
+            itemCount: nowPlayingMovies.length,
+            itemBuilder: (context, index) {
+              final movie = nowPlayingMovies[index];
+              return ListTile(
+                title: Text(movie.title),
+              );
+            },
+          ),
+        )
+      ],
     );
   }
 }
---
+++ moviepediaapp/lib/presentation/widgets/shared/custom_appbar.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final themeOfColors = Theme.of(context).colorScheme;
+    final themeOfStyle = Theme.of(context).textTheme.titleMedium;
+    return SafeArea(
+      bottom: false,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: SizedBox(
+          width: double.infinity,
+          child: Row(
+            children: [
+              Icon(Icons.movie_outlined, color: themeOfColors.primary),
+              const SizedBox(width: 5),
+              Text('MoviePedia', style: themeOfStyle),
+              const Spacer(),
+              IconButton(
+                  onPressed: () {},
+                  color: themeOfColors.primary,
+                  icon: const Icon(Icons.search_outlined))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
