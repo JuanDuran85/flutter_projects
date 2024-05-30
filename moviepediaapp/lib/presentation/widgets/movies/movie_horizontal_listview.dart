1dec37dcfe35e956351fcc81cd9a194da4d230d5
JuanDuran85 <duran85juan@gmail.com>
2024-05-30 13:01:33 -0400

    movies horizontal listview

--- moviepediaapp/lib/presentation/widgets/movies/movie_horizontal_listview.dart
+++ moviepediaapp/lib/presentation/widgets/movies/movie_horizontal_listview.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:moviepediaapp/domain/entities/movie_entities.dart';
 
 class MovieHorizontalListView extends StatelessWidget {
@@ -22,6 +23,57 @@ class MovieHorizontalListView extends StatelessWidget {
       child: Column(
         children: [
           if (title != null || subTitle != null) _Title(title, subTitle),
+          Expanded(
+            child: ListView.builder(
+              itemCount: movies.length,
+              scrollDirection: Axis.horizontal,
+              physics: const BouncingScrollPhysics(),
+              itemBuilder: (context, index) {
+                return _Slide(movie: movies[index]);
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+  final Movie movie;
+  const _Slide({required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            width: 150,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.network(
+                movie.posterPath,
+                fit: BoxFit.cover,
+                width: 150,
+                loadingBuilder: (context, child, loadingProgress) {
+                  if (loadingProgress != null) {
+                    return const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Center(
+                        child: CircularProgressIndicator(
+                          strokeWidth: 2,
+                        ),
+                      ),
+                    );
+                  }
+                  return child;
+                },
+              ),
+            ),
+          ),
         ],
       ),
     );
