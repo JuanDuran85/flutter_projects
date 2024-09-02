c2590b60bf31bdf15490de155d2c602cc280c9e8
JuanDuran85 <duran85juan@gmail.com>
2024-09-01 20:08:19 -0400

    refactor route movies

--- moviepediaapp/lib/config/router/app_router.dart
+++ moviepediaapp/lib/config/router/app_router.dart
@@ -7,22 +7,27 @@ final appRouter = GoRouter(
   initialLocation: '/home/0',
   routes: [
     GoRoute(
-        path: '/home/:page',
-        name: HomeScreen.name,
-        builder: (context, state) {
-          final pageIndex = (state.pathParameters['page'] ?? 0).toString();
-          return HomeScreen(
-            pageIndex: int.parse(pageIndex),
-          );
-        },
-        routes: [
-          GoRoute(
-            path: 'movie/:id',
-            name: MovieScreen.name,
-            builder: (context, state) => MovieScreen(
-              movieId: state.pathParameters['id'] ?? 'no-id',
-            ),
+      path: '/home/:page',
+      name: HomeScreen.name,
+      builder: (context, state) {
+        final pageIndex = (state.pathParameters['page'] ?? 0).toString();
+        return HomeScreen(
+          pageIndex: int.parse(pageIndex),
+        );
+      },
+      routes: [
+        GoRoute(
+          path: 'movie/:id',
+          name: MovieScreen.name,
+          builder: (context, state) => MovieScreen(
+            movieId: state.pathParameters['id'] ?? 'no-id',
           ),
-        ]),
+        ),
+      ],
+    ),
+    GoRoute(
+      path: '/',
+      redirect: (_, __) => '/home/0',
+    ),
   ],
 );
