599f54c378d068d61598f975ebda17a57f45f6e0
JuanDuran85 <duran85juan@gmail.com>
2024-11-29 00:41:53 -0300

    fix favoriti view

--- moviepediaapp/lib/presentation/views/movies/favorites_views.dart
+++ moviepediaapp/lib/presentation/views/movies/favorites_views.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 // ignore: depend_on_referenced_packages
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
+// ignore: depend_on_referenced_packages
+import 'package:go_router/go_router.dart';
 import '../../../domain/domain.dart';
 import '../../presentation.dart';
 
@@ -39,6 +40,37 @@ class FavoritesViewState extends ConsumerState<FavoritesView> {
   Widget build(BuildContext context) {
     final List<Movie> favoritesMovies =
         ref.watch(favoriteMoviesProvider).values.toList();
+
+    if (favoritesMovies.isEmpty) {
+      final ColorScheme themeOfColors = Theme.of(context).colorScheme;
+      return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.favorite_border_sharp,
+              size: 60,
+              color: themeOfColors.primary,
+            ),
+            Text(
+              'Nothings here...',
+              style: TextStyle(color: themeOfColors.primary, fontSize: 30),
+            ),
+            const Text(
+              'No favorite movies',
+              style: TextStyle(fontSize: 20, color: Colors.black45),
+            ),
+            const SizedBox(height: 20),
+            FilledButton.tonal(
+              onPressed: () => context.go('/home/0'),
+              child: const Text('Start to search'),
+            )
+          ],
+        ),
+      );
+    }
+
     return Scaffold(
         body: MovieMasonry(
       movies: favoritesMovies,
